cc5bb2c40b5bc61444d418c246aadcbf7d7ca063
PirialMersus <44650069+PirialMersus@users.noreply.github.com>
2021-02-28 18:02:15 +0300

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img
+                src="https://raw.githubusercontent.com/PirialMersus/frontend-js/master/yds/img/logo.png"
+                alt="logo"/>
+        </header>
+    )
+}
+export default Header
---
+++ src/components/Profile.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img src="https://www.chitalnya.ru/upload3/597/741f859493d647953313397ea2fd5e7d.jpg" alt="girl"/>
+            </div>
+            <div>ava + description</div>
+            <div>my posts
+                <div>New post</div>
+            </div>
+            <div>
+                <div>Post 1</div>
+                <div>Post 2</div>
+                <div>Post 3</div>
+                <div>Post 4</div>
+                <div>Post 5</div>
+            </div>
+        </div>
+    )
+}
+export default Profile
