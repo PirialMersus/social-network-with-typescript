1052a9e144b3cad638cae04cddeaf4ff6621d77d
PirialMersus <44650069+PirialMersus@users.noreply.github.com>
2021-02-28 18:42:44 +0300

    14 lesson

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import s from "./Header.module.css"
 
 const Header = () => {
     return (
-        <header className="header">
+        <header className={s.header}>
             <img
                 src="https://raw.githubusercontent.com/PirialMersus/frontend-js/master/yds/img/logo.png"
                 alt="logo"/>
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import s from "./Profile.module.css"
 
 const Profile = () => {
     return (
-        <div className="content">
+        <div className={s.content}>
             <div>
                 <img src="https://www.chitalnya.ru/upload3/597/741f859493d647953313397ea2fd5e7d.jpg" alt="girl"/>
             </div>
@@ -10,12 +11,12 @@ const Profile = () => {
             <div>my posts
                 <div>New post</div>
             </div>
-            <div>
-                <div>Post 1</div>
-                <div>Post 2</div>
-                <div>Post 3</div>
-                <div>Post 4</div>
-                <div>Post 5</div>
+            <div className={s.posts}>
+                <div className={s.item}>Post 1</div>
+                <div className={s.item}>Post 2</div>
+                <div className={s.item}>Post 3</div>
+                <div className={s.item}>Post 4</div>
+                <div className={s.item}>Post 5</div>
             </div>
         </div>
     )
